48b6c16269d2be3eff4593d6cb228d2ce275e2ec
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2024-12-25 01:46:41 +0100

    nionde

--- Vinterprojekt/Program.cs
+++ Vinterprojekt/Program.cs
@@ -23,26 +23,59 @@ List<string> attcakHit = ["Slime spottar en svag syra på dig", "Greater Slime h
 List<string> attcakMiss = ["Slimet hoppar mot dig och du undvek precis i tid", "Greater Slime gör en stor syr boll och kastar den emot dig men du lyckades ta skydd i tid", "Slime kungen på börjar en magisk attack men du påminner den att den inte kan magi"];
 int level = 1;
 
-// påverkar bådas chans att träffa
-int chance = 0;
+int chance = 0; // påverkar bådas chans att träffa
 
-// om spelaren vill spela igen
-bool playAgain = true;
+bool playAgain = true; // om spelaren vill spela igen
+
+// gör det mer intreserand att läsa början igen plus ger bakgrund till hur du fick alla saker
+List<string> story = ["Du är en äventyrare som har i uppgift att undersöka och töma en fängelsehåla(duengon) på skater och monster!", "Din far var en äventyrare som skulle undersöka en fängelsehåla men han återvände aldrig. En dag kom en annan äventyrare med alla din fars ägodelar och du bästemer dig för att avsluta det din far hade startat.", "Senda du var ung så hade du hör att din farfar hade ofrat sitt liv för din fars bästa och en dag begav sig din far att göra det samma och när du fick din fars ägodelar så viste du redan vad som behövde ske.",];
+int storyProgres = 0;
 
 while (playAgain == true)
 {
-    playerArmor = 1;
+    // gör så att spelaren inte startar död eller startar mot ett svårare monster än menat
     playerHealth = 100;
-
-    item = 0;
-    gold = 0;
-
     level = 1;
 
     spel();
-    if (playerHealth < 0)
+    if (playerHealth <= 0)
     {
+        storyProgres++;
+
         Console.WriteLine("Du dog. Vill du försöka igen? Y/N");
+        string yesNo = Console.ReadLine().ToUpper();
+
+        while (yesNo != "Y" && yesNo != "N")
+        {
+            Console.WriteLine("skriv Y för ja eller N för nej");
+            yesNo = Console.ReadLine().ToUpper();
+        }
+
+        if (yesNo == "Y")
+        {
+            playAgain = true;
+            Console.WriteLine();
+            Console.WriteLine($"Du har {gold} guld, {playerArmor} skydd(armor) och {item} hälsodrycker");
+        }
+        else
+        {
+            playAgain = false;
+        }
+
+        yesNo = "";
+        Console.WriteLine();
+    }
+
+    if (playerHealth > 0)
+    {
+        // gör så att man startar om totalt från början
+        playerArmor = 1;
+        playerAttack = 10;
+        item = 0;
+        gold = 0;
+        storyProgres = 0;
+
+        Console.WriteLine("Du besegrade Slime kungen. Vill du slå han igen? Y/N");
         string yesNo = Console.ReadLine().ToUpper();
 
         while (yesNo != "Y" && yesNo != "N")
@@ -62,32 +95,11 @@ while (playAgain == true)
         yesNo = "";
         Console.WriteLine();
     }
-
-    if (playerHealth > 0)
-    {
-        Console.WriteLine("Du besegrade Slime kungen. Vill du slå han igen? Y/N");
-        string yesNo = Console.ReadLine().ToUpper();
-
-        while (yesNo != "Y" && yesNo != "N")
-        {
-            Console.WriteLine("skriv Y för ja eller N för nej");
-            yesNo = Console.ReadLine().ToUpper();
-        }
-
-        if (yesNo == "Y")
-        {
-            playAgain = true;
-        }
-        else
-        {
-            playAgain = false;
-        }
-    }
 }
 
 void spel()
 {
-    Console.WriteLine("Du är en äventyrare som har i uppgift att undersöka och töma en fängelsehåla(duengon) på skater och monster!");
+    Console.WriteLine(story[storyProgres]);
     Console.WriteLine("I det första rummet finns det en skat kista och en dör. vill du öppna kistan? Y/N");
     string yesNo = Console.ReadLine().ToUpper();
 
@@ -103,7 +115,7 @@ void spel()
         Console.WriteLine("Du öppna kistan och hittar några stål hanskar, ditt försvar går up med 3.");
         Console.WriteLine("Du hittar även en hälsodryck, efter det går du igenom dörren");
         playerArmor += 3;
-        item ++;
+        item++;
     }
     else
     {
@@ -133,6 +145,10 @@ void spel()
             Console.WriteLine("Du går mott metal dörren och hör något bankande på dörren, när du öppna den så kom");
             Console.WriteLine("en stor Slime i hög far rakt emot dig och misade dig med ett hårstrå och framför dig stog en");
             fight();
+            if (playerHealth > 0)
+            {
+                shop();
+            }
         }
         else
         {
@@ -141,19 +157,30 @@ void spel()
 
     }
 
+    if (playerHealth > 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Efter du hndlade det du fan viktigt så fortaste du till nästa rum.");
+        Console.WriteLine("När du kom in i rummet så märkte du att rummet var väldigt öppen nästan som ett tron rum för en kung");
+        Console.WriteLine("det är då du la märke till att en stor skugga satt på en torn i längst back i rummet och du mötte ");
+
+        fight();
+    }
+
 }
 
 
 void fight()
 {
     monster(level); // ger monstret hur mycket HP, DEF och ATK som det ska ha
-    playerChois = 0; // gör så att programet inte 
+    playerChois = 0; // gör så att tidigare Chois inte påverkar striden
+
     while (playerHealth > 0 && monsterHealth > 0 && theEscape == false)
     {
         bool success = false;
         while (playerTurn == true && playerHealth > 0 && monsterHealth > 0 && theEscape == false)
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++) // ger ett mellan rum från texten innan striden startade och för texten som säger att du missa eller träffa
             {
                 Console.WriteLine();
             }
@@ -207,7 +234,7 @@ void fight()
             if (playerMove == 2) // om spelaren väljer försvar
             {
                 playerDefens = 2 * playerArmor;
-                Console.WriteLine($"1. skyda dig (tar {playerDefens} minder dmg)  2. backa");
+                Console.WriteLine($"1. skyda dig (tar {playerDefens + playerArmor} minder dmg)  2. backa");
                 while (playerChois > 2 || playerChois < 1)
                 {
                     player = Console.ReadLine();
@@ -284,11 +311,22 @@ void fight()
                 playerMove = 0;
                 playerChois = 0;
                 chance = Random.Shared.Next(1, 11);
-                if (chance > 3)
+
+                if (chance > 3 && monsterHealth > 0)
                 {
                     Console.Write(attcakHit[level - 1]);
-                    Console.WriteLine($" och gör {monsterAttack - (playerArmor + playerDefens)} skada");
-                    playerHealth -= monsterAttack - (playerArmor + playerDefens);
+                    if (monsterAttack - (playerArmor + playerDefens) < 0) // gör så att monstret inte kan heala spelaren 
+                    {
+                        monsterAttack = playerArmor + playerDefens;
+                        Console.WriteLine($" och gör {monsterAttack - (playerArmor + playerDefens)} skada");
+                        playerHealth -= monsterAttack - (playerArmor + playerDefens);
+                        monsterAttack = 15 * level;
+                    }
+                    else
+                    {
+                        Console.WriteLine($" och gör {monsterAttack - (playerArmor + playerDefens)} skada");
+                        playerHealth -= monsterAttack - (playerArmor + playerDefens);
+                    }
                 }
                 else
                 {
@@ -334,7 +372,7 @@ void monsterAppearance(int level)
     {
         Console.WriteLine();
         Console.WriteLine("Greater Slime ( *-* )");
-        Console.WriteLine($"ATK {monsterAttack - 5} HP {monsterHealth} DEF {monsterDefens}");
+        Console.WriteLine($"ATK {monsterAttack} HP {monsterHealth} DEF {monsterDefens}");
         Console.WriteLine();
     }
 
@@ -342,13 +380,72 @@ void monsterAppearance(int level)
     {
         Console.WriteLine();
         Console.WriteLine("           ^^^^^^^   ");
-        Console.WriteLine("Slime KING(  *-*  )");
-        Console.WriteLine($"ATK {monsterAttack - 15} HP {monsterHealth} DEF {monsterDefens}");
+        Console.WriteLine("Slime KING( *___* )");
+        Console.WriteLine($"ATK {monsterAttack} HP {monsterHealth} DEF {monsterDefens}");
         Console.WriteLine();
     }
 }
 
 void shop()
 {
+    Console.WriteLine();
+    Console.WriteLine("Du rör dig mott trä dörren och hör en röst mumlande på andra sidan.");
+    Console.WriteLine("När du öppnar dörren blir du bemöt av en eldre man som verkar driva en affär här och säljer.");
+    List<string> shopItems = ["1. hälsodrycker", "2. skydd (+3 armor)", "3. slipsten (+10 dmg)"];
+    List<int> itemPrices = [3, 7, 10];
+
+    while (playerChois != 4)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"du har {gold} guld");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(shopItems[i]);
+            Console.Write(" för ");
+            Console.Write(itemPrices[i]);
+            Console.WriteLine(" guld");
+        }
+            Console.WriteLine("4. lämna affär");
+
+        bool success = false;
+        while (success == false || playerChois > 4 || playerChois < 0)
+        {
+            player = Console.ReadLine();
+            success = int.TryParse(player, out playerChois);
+        }
+
+        if (playerChois == 1 && gold > 2)
+        {
+            Console.WriteLine("Du köpte en hälsodryk för 3 guld.");
+            item ++;
+            gold -= 3;
+        }
+        else if (playerChois == 1 && gold < 2)
+        {
+            Console.WriteLine("Du har inte råd");
+        }
+        
+        if (playerChois == 2 && gold > 5)
+        {
+            Console.WriteLine("Du köpte skydd för 7 guld.");
+            playerArmor += 3;
+            gold -= 7;
+        }
+        else if (playerChois == 2 && gold < 5)
+        {
+            Console.WriteLine("Du har inte råd");
+        }
+
+        if (playerChois == 3 && gold > 10)
+        {
+            Console.WriteLine("Du köpte en slipsten för 10 guld.");
+            playerAttack += 10;
+            gold -= 10;
+        }
+        else if (playerChois == 3 && gold < 10)
+        {
+            Console.WriteLine("Du har inte råd");
+        }
+    }
 
 }
